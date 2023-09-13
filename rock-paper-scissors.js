b75fa3504a315796df1073dc3bad9d4ea3c82602
willcornforth <141945840+willcornforth@users.noreply.github.com>
2023-09-13 11:01:52 +0100

    Add GUI
    - Added buttons for playing game.
    - Added divs for round result output.
    - Reworked game logic to reduce code size.
    - Remove mainGame() and getUserChoice().

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,99 +1,77 @@
 console.log("[+] Starting rock paper scissors...");
 
+const Choices = 
+{
+    Rock: 0,
+    Paper: 1,
+    Scissors: 2
+}
+let userScore = 0;
+
 // Generates random choice from pseudo-random number.
 function getRandomChoice()
 {
-    let random = Math.round( Math.random() * 2 ); // Returns random number between 1 and 3 and round result.
+    let random = Math.floor( Math.random() * 3 ); // Returns random number between 0 and 2 and round result.
     console.log("[+] Random number: " + random);
 
-    // Get return string for randomly generated number.
-    switch( random )
-    {
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors";
-        default:
-            console.log("[-] getRandomChoice() failed with random: " + random);
-            return "ERROR";
-    }
-}
-
-// Poll console for user's choice.
-function getUserChoice()
-{
-    let input = prompt("Enter either 'Rock', 'Paper' or 'Scissors'");
-    switch( input )
-    {
-        case "Rock":
-            return input;
-        case "Paper":
-            return input;
-        case "Scissors":
-            return input;
-        default:
-            console.log("[-] getUserChoice() failed with bad input: " + input);
-            return "ERROR";
-    }
+    return random;
 }
 
 //Plays a game of rock paper scissors.
-function playRound(userChoice, opponentChoice)
+function playRound(userNum, opponentNum)
 {
-    // Generate key value pairs of choices.
-    let choiceKeyValue = {
-        "Rock": 0,
-        "Paper": 1,
-        "Scissors": 2,
-    }
+    let result = 0;
 
-    // Convert string choice to integer.
-    let userNum = choiceKeyValue[userChoice];
-    let opponentNum = choiceKeyValue[opponentChoice];
+    // Check to see if we have drawn.
+    if(userNum == opponentNum)
+        result = -1;
 
-    // Check if our user has won the round, if not return false;
-    if(userNum == 1 && opponentNum == 0)
-        return true;
-    else if(userNum == 2 && opponentNum == 1)
-        return true;
-    else if(userNum == 0 && opponentNum == 2)
-        return true;
-
-    return false;
-}
-
-function mainGame()
-{
-    let roundsWon = 0; let totalRounds = 5;
-    for(let i = 0; i < totalRounds; i++)
+    if(result != -1)
     {
-        // Poll user's choice.
-        let userChoice = getUserChoice();
-
-        // Handle bad input.
-        if(userChoice == "ERROR")
-            return;
-
-        console.log("[+] User inputted choice: " + userChoice);
-
-        // Generate random opponent choice.
-        let opponentChoice = getRandomChoice();
-        console.log("[+] Computer generated choice: " + opponentChoice);
-
-        // Play one round with new inputs.
-        let roundResult = playRound(userChoice, opponentChoice);
-        console.log("[#] Round " + i + " Result: " + roundResult);
-
-        // Round won, increment score
-        if(roundResult)
-            roundsWon++;
+        result = (userNum == Choices.Paper && opponentNum == Choices.Rock) 
+        || (userNum == Choices.Scissors && opponentNum == Choices.Paper) 
+        || (userNum == Choices.Rock && opponentNum == Choices.Scissors);
     }
 
-    // Print game results.
-    console.log("[+] You won " + roundsWon + " out of " + totalRounds + " rounds.");
+    postRound(userNum, opponentNum, result);
+
+    return result;
 }
 
-// Run game.
-mainGame();
+document.querySelector('#btn-rock').onclick = () => {
+    playRound(Choices.Rock, getRandomChoice());
+};
+
+document.querySelector('#btn-paper').onclick = () => {
+    playRound(Choices.Paper, getRandomChoice());
+};
+
+document.querySelector('#btn-scissors').onclick = () => {
+    playRound(Choices.Scissors, getRandomChoice());
+};
+
+function postRound(userChoice, oppChoice, result)
+{
+    const choicePairs = 
+    {
+        0: "Rock",
+        1: "Paper",
+        2: "Scissors"
+    }
+
+    const divResults = document.querySelector('#div-results');
+    if(result != -1)
+    {
+        // Increment score unless score is already zero.
+        result ? userScore++ : userScore < 1 ? 0 : userScore--;
+
+        divResults.textContent = "User chose: " + choicePairs[userChoice] 
+            + " Opponent chose: " + choicePairs[oppChoice];
+    }
+    else {
+        divResults.textContent = "Draw!";
+    }
+
+    const divScore = document.querySelector('#div-score');
+    divScore.textContent = "Score: " + userScore;
+}
